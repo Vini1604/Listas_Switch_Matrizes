e66d451d865e1e78f952e1501a41041fe9bc462b
Vini1604 <vinicius.oliveira1998@hotmail.com>
2022-06-08 09:30:17 -0300

    Acrescentado mensagens ao Exercicio de Matrizes

--- Matrizes/Program.cs
+++ Matrizes/Program.cs
@@ -31,23 +31,30 @@ namespace Matrizes
                 somaLinha1 += campoMinado[0, i];
                 
             }
-            Console.WriteLine(somaLinha1);
+            Console.WriteLine($"Soma dos elementos da linha 1: {somaLinha1}");
+            Console.WriteLine();
 
             for (int i = 0; i < campoMinado.GetLength(1); i++)
             {
                 somaColuna2 += campoMinado[i, 1];
             }
-            Console.WriteLine(somaColuna2);
+            Console.WriteLine($"Soma dos elementos da coluna 2: {somaColuna2}");
+            Console.WriteLine();
 
             multiplicaCelulas = bandeira * campoMinado[1, 2];
-            Console.WriteLine(multiplicaCelulas);
+            
+            Console.WriteLine($"Multiplicacao da bandeira pela celula [1, 2]:  {multiplicaCelulas}");
+            Console.WriteLine();
 
 
+            Console.WriteLine("Divisao da bandeira pela bomba ...");
+            Console.WriteLine("NAO E POSSIVEL A DIVISAO POIS TEMOS UMA DIVISAO POR ZERO");
             // NAO E POSSIVEL A DIVISAO POIS TEMOS UMA DIVISAO POR ZERO
             /*  
             divideBomba = bandeira / bomba;
             Console.WriteLine(divideBomba);
             */
+
         }
     }
 }
